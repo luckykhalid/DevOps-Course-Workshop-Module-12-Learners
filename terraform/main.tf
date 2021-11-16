266b8362de2a8af20910e8e919499c4bd3664d5d
Khalid Ashraf <luckykhalid@gmail.com>
2021-11-16 14:48:14 +0000

    Workshop afternoon session Step 3: Variables and secrets

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+variable "database_password" {
+  description = "Database password"
+  sensitive   = true
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "main" {
+  name = "OpenCohort1_KhalidAshraf_Workshop_M12_Pt2"
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "khalid-terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "khalid-terraformed-app-service"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
+
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|corndelldevopscourse/mod12app:latest"
+  }
+
+  app_settings = {
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" : "True"
+    "DEPLOYMENT_METHOD" : "Terraform"
+    "CONNECTION_STRING" : "Server=tcp:khalidashraf-non-iac-sqlserver.database.windows.net,1433;Initial Catalog=khalidashraf-non-iac-db;Persist Security Info=False;User ID=db;Password=${var.database_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+
+}
